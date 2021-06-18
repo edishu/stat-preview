7cdf4afbdb126fdde84c6efcb6ddd508f6003232
Udayan Maurya <edmauryaishu@gmail.com>
2021-06-18 19:48:11 -0400

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: () => ({
+        desktopImg: "url('/image-header-desktop.jpg')",
+        mobileImg: "url('/image-header-mobile.jpg')"
+      }),
+      colors: {
+        backBlue: "hsl(233, 47%, 7%)",
+        cardBlue: "hsl(244, 38%, 16%)",
+        softViolet: "hsl(277, 64%, 61%)"
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
